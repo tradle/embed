f67d81282461705033648b3af45294423fa985f0
Martin Heidegger <martin.heidegger@gmail.com>
2022-02-27 00:38:50 +0900

    chore: adding missing s3 endpoints

--- index.js
+++ index.js
@@ -40,7 +40,19 @@ const S3_ENDPOINTS = {
   'ap-southeast-1': 's3-ap-southeast-1.amazonaws.com',
   'ap-southeast-2': 's3-ap-southeast-2.amazonaws.com',
   'ap-northeast-1': 's3-ap-northeast-1.amazonaws.com',
-  'cn-north-1': 's3.cn-north-1.amazonaws.com.cn'
+  'cn-north-1': 's3.cn-north-1.amazonaws.com.cn',
+  'af-south-1': 's3.af-south-1.amazonaws.com',
+  'ap-east-1': 's3.ap-east-1.amazonaws.com',
+  'ap-southeast-3': 's3.ap-southeast-3.amazonaws.com',
+  'ap-northeast-3': 's3.ap-northeast-3.amazonaws.com',
+  'ap-northeast-2': 's3.ap-northeast-2.amazonaws.com',
+  'ap-southeast-2': 's3.ap-southeast-2.amazonaws.com',
+  'eu-south-1': 's3.eu-south-1.amazonaws.com',
+  'eu-west-3': 's3.eu-west-3.amazonaws.com',
+  'eu-north-1': 's3.eu-north-1.amazonaws.com',
+  'me-south-1': 's3.me-south-1.amazonaws.com',
+  'us-gov-east-1': 's3.us-gov-east-1.amazonaws.com',
+  'us-gov-west-1': 's3.us-gov-west-1.amazonaws.com',
   // Add new endpoints here.
 }
 
